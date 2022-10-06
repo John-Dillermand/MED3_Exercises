832527f5a9317f7fb7941b9e8c6c3dadc9b79e7c
John-Dillermand <ctuk21@student.aau.dk>
2022-10-06 12:08:56 +0200

    udaet

--- Exercise 5 folder/Blurring.py
+++ Exercise 5 folder/Blurring.py
@@ -3,40 +3,37 @@ from turtle import width
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('Circles.png')
-width, height = img.shape[0], img.shape[1]
-
-
-structuringElement = np.ones((3,3))
+img = cv.imread('Circles.png',0)
+height, width = img.shape[0], img.shape[1]
 
 
 
-kernel = np.ones((3,3))
+
+kernel = np.ones((11,11))
 
 
-offset = len(structuringElement) //2
+offset = len(kernel) //2
 
 hitTest = np.zeros((3,3))
 hitTest[0,0] = 1
 
-blurredImage = np.zeros((width,height),np.uint8)
-
-
+blurredImage = np.zeros((height,width),np.uint8)
 
 for x in range(offset,width-offset):
     for y in range(offset,height-offset):
-        acc = [0,0,0]
+        pixelValue=0
         for a in range (len(kernel)):
             for b in range(len(kernel)):
                 xn = x + a - offset
                 yn = y + b  - offset
-                pixel = img[xn, yn]
-                acc[0] += pixel[0] * kernel[a,b]
-                acc[1] += pixel[1] * kernel[a,b]
-                acc[2] += pixel[2] * kernel[a,b]
+                pixelValue += img[yn,xn] * kernel[a,b] 
 
 
+        blurredImage[yn,xn]=pixelValue /9
 
+
+cv.imshow("Image", blurredImage)
+cv.waitKey(0)
 
 
 #print(acc)
@@ -62,10 +59,9 @@ for x in range(offset,width-offset):
 #print(structuringElement==fitTest)
 #print(np.all(structuringElement==fitTest))
 #print(np.all(structuringElement==hitTest))
-print(offset)
 
-#cv.imshow("Image",img)
-cv.waitKey(0)
+
+
 #Ide til hvordan jeg skal lave Dilation and erosion
 
 #Jeg skal bare kunne trække en matrix af 3x3 størrelse ud af en bestemt center pixel.
