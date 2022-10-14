0233423e9097937dc34ddd640efe7b0abb161b62
John-Dillermand <ctuk21@student.aau.dk>
2022-10-14 13:37:45 +0200

    Update

--- KingDominion/CrownRecognition.py
+++ KingDominion/CrownRecognition.py
@@ -1,14 +1,15 @@
+from ctypes import sizeof
 import cv2 as cv
 import numpy as np
 import math
 from numpy import array
 import math
+import linecache
 
-FileName = "YellowBlank3.png"
+FileName = "LabeledData\YellowBlank1.png"
 crown    = 0
 crown = str(crown)
-enumerate
-sand = 0
+
 regions = ["sand", "water", "grass"]
 
 img = cv.imread(FileName)
@@ -17,9 +18,7 @@ img = cv.imread(FileName)
 height = img.shape[0]
 width = img.shape[1]
 
-str1 = "Test1"
-str2 = "Test2"
-str3 = "Test3"
+
 
 
 Red = img[:, :, 2]
@@ -33,29 +32,73 @@ averageBlue   = str(((np.sum(Blue)) /(height*width)))
 
 averageRGB = ["RED=",averageRed,"GREEN=",averageGreen,"BLUE=",averageBlue]
 
-printToData = ["FileName=",FileName,",""RED=",averageRed,",","GREEN=",averageGreen,",","BLUE=",averageBlue,",",",crown=",crown]
+currentAverageRed = 0
+currentAverageGreen = 0
+currentAverageBlue = 0
+
+
+
+class Tile:
+    def __init__(self, name, R, G, B, crowns, region):
+        self.name = name
+        self.R = R
+        self.G = G
+        self.B = B
+        self.crowns = crowns
+        self.region = region
+
+
+# Making the tile
+currentTile = Tile(FileName,averageRed,averageGreen,averageBlue,crown,"sand")
+
+
+
+
+
+
+
 
 
 
 
 
 #printToData = ["FileName=",FileName,",""RED=",averageRed,",","GREEN=",averageGreen,",","BLUE=",averageBlue,",","region=",",","Crown=",crown]
-dataSheet = open("dataSheet", "r+")
+dataSheet = open("dataSheet", "w")
+
+printToData = ["FileName=",currentTile.name,"\n"
+                "RED=",currentTile.R,"\n",
+                "GREEN=",currentTile.G,"\n",
+                "BLUE=",currentTile.B,"\n",
+                "crown=",str(currentTile.crowns),"\n",
+                "region=",currentTile.region,"\n"]
 
 dataSheet.writelines(printToData)
 
+dataSheet.close()
+
+
+dataSheet = open("dataSheet", "r")
+
+currentLine = dataSheet.read()
+#print(currentLine)
+#print(currentLine.find("RED="))
 
 
 
+dataSheet.read(38)
 
 dataSheet.close()
 
-print(averageRGB)
 
 
+nextLine=0
+currentAverageRed = linecache.getline("dataSheet",2+nextLine)
+currentAverageGreen = linecache.getline("dataSheet",3+nextLine)
+currentAverageBlue = linecache.getline("dataSheet",4+nextLine)
 
 
-#cv.imshow("Image",img)
-#cv.waitKey(0)
+#getAveragesFromFile()
 
-#print(bayerOutput)
+print(currentAverageRed)
+print(currentAverageGreen)
+print(currentAverageBlue)
