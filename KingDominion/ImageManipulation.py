6713f68ac248dc318a216ae2607868b1eb3306b0
John-Dillermand <ctuk21@student.aau.dk>
2022-10-26 18:31:18 +0200

    update

--- KingDominion/ImageManipulation.py
+++ KingDominion/ImageManipulation.py
@@ -1,33 +1,91 @@
+from xml.dom.minidom import Element
+import numpy as np
 import cv2 as cv
 
-img = cv.imread('1.jpg',0)
+img = cv.imread("1.jpg")
 
-def nothing(x):
-    pass
+#initialise variables with a default
+lowerTresh = 0
+higherTresh = 255
 
-cv.namedWindow("image")
+bino_thresh     = 5
+canny_thresh    = 5
+dialation       = 0
+erode           = 0
 
-#Create TrackBars
-cv.createTrackbar('LowThresh', 'image', 0, 254, nothing)
-cv.createTrackbar('HighThresh', 'image', 0, 255, nothing)
+# functions that update a variable, 
+# then call image processing function
+def change_lowerTresh(val):
+    global lowerTresh
+    lowerTresh = val
+    thresh_callback()
+
+#def change_higherTresh(val):
+#    global higherTresh
+#    higherTresh = val
+#    thresh_callback()
+
+def change_bino(val):
+    global bino_thresh
+    bino_thresh = val
+    thresh_callback()
+
+def change_dialation(val):
+    global dialation
+    dialation = val
+    thresh_callback()
+
+def change_erode(val):
+    global erode
+    erode = val
+    thresh_callback()
 
 
-switch = 'OFF-ON'
-cv.createTrackbar(switch, 'image',0,1,nothing)
+
+def grayScale(val):
+    imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# your function that processes the image
 
 
-while(1):
-    cv.imshow("image",img)
-    k = cv.waitKey(1) & 0xFF
-    if k == 27:
-        break
 
-    #Get the current positions of the track bars
-    lowThresh = cv.getTrackbarPos("LowThresh" ,"image")
-    highThresh= cv.getTrackbarPos("HighThresh","image")
-    s         = cv.getTrackbarPos(switch,"image")
+source_window = 'Source'
+cv.namedWindow(source_window)
 
 
-    img[:] = [lowThresh,highThresh]
+def thresh_callback():
+    imggray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+    cv.imshow("Source",imggray)
+    
+    ret,imggray = cv.threshold(imggray,lowerTresh,higherTresh,cv.THRESH_BINARY)
+    imgErode     = cv.erode(src=imggray,dst=None,kernel=cv.getStructuringElement(cv.MORPH_RECT,(3,3)),iterations=erode)
+    imgDialat     = cv.dilate(src=imgErode,dst=None,kernel=cv.getStructuringElement(cv.MORPH_RECT,(3,3)),iterations=dialation)
+
+
+
+
+
+    cv.imshow("Source",imgDialat)
+
+
+
+
+
+
+
+
+
+max_thresh = 255
+thresh = 100
+# create trackbars
+cv.createTrackbar('Thres:', source_window, thresh, max_thresh, change_lowerTresh)
+#cv.createTrackbar('higherTresh:', source_window, thresh, max_thresh, change_higherTresh)
+cv.createTrackbar('Erode:', source_window, 0, 5, change_erode)
+cv.createTrackbar('Dialation:', source_window, 0, 5, change_dialation)
+
+
+thresh_callback()
+
+cv.waitKey(0) & 0xFF  
 cv.destroyAllWindows()
